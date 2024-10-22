3526cf89840d411a0e50cb7725d6f7c238ee258a
Philip Jagielski <pjagielski@apple.com>
2024-10-22 14:43:51 -0500

    Add nvim-surround plugin

--- init.lua
+++ init.lua
@@ -778,7 +778,7 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- require('mini.surround').setup() -- use nvim-surround instead
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
@@ -890,6 +890,12 @@ require('lazy').setup({
         -- v = { k = { j = '<Esc>' } },
       },
     },
+  },
+  {
+    'kylechui/nvim-surround',
+    version = '*',
+    event = 'VeryLazy',
+    opts = {},
   },
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
