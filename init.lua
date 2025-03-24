592c17af2fcc36be39a24968d952efb0a202cbc1
Philip Jagielski <pjagielski@apple.com>
2025-03-24 17:16:57 -0500

    Show hidden files (start with .) except .git/ and .vscode/

--- init.lua
+++ init.lua
@@ -348,12 +348,15 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          file_ignore_patterns = { '%.git/', '%.vscode/' },
+          mappings = {
+            i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          },
+        },
+        pickers = {
+          find_files = { hidden = true },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
