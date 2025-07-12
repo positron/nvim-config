23aae0997383c968a1dc0f729dff06969dd6c15f
Philip Jagielski <pjagielski@apple.com>
2025-07-11 22:25:17 -0500

    Add todo to try out nvim rest plugin

--- init.lua
+++ init.lua
@@ -1020,6 +1020,27 @@ require('lazy').setup({
 
   require 'kickstart.plugins.neo-tree',
 
+  -- {
+  --   'rest-nvim/rest.nvim',
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --     opts = function(_, opts)
+  --       opts.ensure_installed = opts.ensure_installed or {}
+  --       table.insert(opts.ensure_installed, 'http')
+  --     end,
+  --   },
+  --   build = function()
+  --     vim.cmd 'TSInstall http'
+  --   end,
+  -- },
+  -- { -- dependency of rest.nvim
+  --   'vhyrro/luarocks.nvim',
+  --   name = 'luarocks',
+  --   opts = {
+  --     rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua' },
+  --   },
+  -- },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
