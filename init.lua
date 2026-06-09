689b0878a63fd29310ff1e64540182b1ee0282c2
Philip Jagielski <philipjagielski@gmail.com>
2026-06-09 14:52:39 -0500

    Fix tree sitter, update to main branch and reinstall stuff
    Had to `brew install tree-sitter-cli`

--- init.lua
+++ init.lua
@@ -1043,11 +1043,11 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = {
+    config = function()
+      local ensure_installed = {
         'clojure',
         'java',
         'javascript',
@@ -1068,7 +1068,6 @@ require('lazy').setup({
         'editorconfig',
         'tmux',
         'bash',
-        'diff',
         'html',
         'lua',
         'luadoc',
@@ -1077,24 +1076,22 @@ require('lazy').setup({
         'query',
         'vim',
         'vimdoc',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      }
+      require('nvim-treesitter').install(ensure_installed)
+
+      local filetypes = {}
+      for _, lang in ipairs(ensure_installed) do
+        vim.list_extend(filetypes, vim.treesitter.language.get_filetypes(lang))
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = filetypes,
+        callback = function(args)
+          vim.treesitter.start()
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
   },
 
   -- pkl isn't automatically supported by treesitter, so they have this whole snippet to install it
@@ -1102,14 +1099,6 @@ require('lazy').setup({
     'apple/pkl-neovim',
     lazy = true,
     ft = 'pkl',
-    dependencies = {
-      {
-        'nvim-treesitter/nvim-treesitter',
-        build = function(_)
-          vim.cmd 'TSUpdate'
-        end,
-      },
-    },
     build = function()
       require('pkl-neovim').init()
 
